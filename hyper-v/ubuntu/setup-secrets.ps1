6a08462c5edf9e84c485cd453365dc604bde604c
Vitaly Andrev <v.andreev@systtech.ru>
2026-04-29 20:26:59 +0700

    chore: up req versions

--- hyper-v/ubuntu/setup-secrets.ps1
+++ hyper-v/ubuntu/setup-secrets.ps1
@@ -52,7 +52,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 `
     -Scope CurrentUser -Force -ForceBootstrap | Out-Null
 $_common = Get-Module -ListAvailable -Name Infrastructure.Common |
     Sort-Object Version -Descending | Select-Object -First 1
-if (-not $_common -or $_common.Version -lt [Version]'1.3.3') {
+if (-not $_common -or $_common.Version -lt [Version]'2.0.0') {
     Install-Module Infrastructure.Common -Scope CurrentUser -Force
 }
 Import-Module Infrastructure.Common -Force -ErrorAction Stop
@@ -63,7 +63,7 @@ Import-Module Infrastructure.Common -Force -ErrorAction Stop
 . "$PSScriptRoot\common\config\ConvertFrom-VmConfigJson.ps1"
 
 # The minimum version is pinned here - bump it when a newer feature is required.
-Invoke-ModuleInstall -ModuleName 'Infrastructure.Secrets' -MinimumVersion '2.1.0'
+Invoke-ModuleInstall -ModuleName 'Infrastructure.Secrets' -MinimumVersion '3.0.0'
 
 Initialize-MicrosoftPowerShellSecretStoreVault `
     -VaultName           'VmProvisioner' `
